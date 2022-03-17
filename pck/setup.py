e4d3aa9f4810bf2360eb9079245a1b9378e3b192
Disalo <p.siyahpoosh@gmail.com>
2022-03-17 14:30:22 +0100

    add python package

---
+++ pck/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+    name='mrecomm',
+    version='0.0.1',
+    author='Payam',
+    author_email='p.siyahpoosh@gmail.com',
+    packages=['pck'],
+    #url='https://github.com/Ajax121/movie_recomm',
+    license='MIT',
+    description='A very simple movie recommender that gives a random movie', 
+    install_requires = ['pandas','numpy'],
+                                
+)
